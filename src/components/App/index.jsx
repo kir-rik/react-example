0994087f09a9f2f776edb950cc1d17da8f9b1866
kir_rik <kir-rik-90@yandex.ru>
2020-08-16 22:30:51 +0400

    handleClick

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,11 +3,29 @@ import Text from '../Text';
 import Button from '../Button';
 
 export default class App extends React.PureComponent {
+  state = {
+    text1: 'Какой-то текст',
+    text2: 'Какой-то другой текст',
+    text3: 'Какой-то третий текст',
+  };
+
+  handleClick = () =>
+    this.setState((state, props) => {
+      return {
+        text1: state.text1 + '1',
+        text2: 'Поклацано',
+      };
+    });
+
   render() {
+    const { text1, text2, text3 } = this.state;
+  
     return (
       <main>
-        <Text value="Какой-то текст" />
-        <Button title="Какая-то кнопка" handleClick={() => console.log('Клац')} />
+        <Text value={text1} />
+        <Text value={text2} />
+        <Text value={text3} />
+        <Button title="Какая-то кнопка" handleClick={this.handleClick} />
       </main>
     );
   }
