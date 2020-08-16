b3473dde8e1a24bf8a42927df54b5b821ec7b64d
kir_rik <kir-rik-90@yandex.ru>
2020-08-16 21:42:04 +0400

    props

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
-import HelloWorld from './components/HelloWorld'
+import App from './components/App'
 
-ReactDOM.render(<HelloWorld />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
