b9afe75e4b5aa6aa3605df15d37d337fc2d30caa
kir_rik <kir-rik-90@yandex.ru>
2020-08-18 22:59:14 +0400

    lifecycle

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,21 +1,48 @@
 import React from 'react';
 import Text from '../Text';
-import Loader from '../Loader';
+import Button from '../Button';
 
 export default class App extends React.PureComponent {
-  constructor(props) {
-    super(props);
+  state = {
+    value: 0,
+  };
 
-    this.state = {
-      isLoadig: true,
-    };
-  }
+  handleClick = () =>
+    this.setState((state, props) => {
+      return {
+        value: state.value + 1,
+      };
+    });
 
-  completeLoading = () => this.setState(() => ({ isLoadig: false }));
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log('%c%s', 'background-color: yellow', `Before every render. State: ${JSON.stringify(state)}`);
+  //   return null;
+  //   // return state.value > 5
+  //   //   ? {
+  //   //       value: 42,
+  //   //     }
+  //   //   : null;
+  // }
+
+  // componentDidMount() {
+  //   console.log('%c%s', 'background-color: orange', `After initial render. State: ${JSON.stringify(this.state)}`);
+  // }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log('%c%s', 'background-color: #00CED1', `After non-initial render. ${prevState.value} -> ${this.state.value}`);
+  // }
 
   render() {
-    const { isLoadig } = this.state;
+    console.log('%c%s', 'background-color: #F0FFF0', `Render. State: ${JSON.stringify(this.state)}`);
+  
 
-    return isLoadig ? <Loader handleClick={this.completeLoading} /> : <Text value="Загружено" />;
+    const { value } = this.state;
+
+    return (
+      <main>
+        <Text value={`Какой-то текст ${value}`} />
+        <Button title="Какая-то кнопка" handleClick={this.handleClick} />
+      </main>
+    );
   }
 }
